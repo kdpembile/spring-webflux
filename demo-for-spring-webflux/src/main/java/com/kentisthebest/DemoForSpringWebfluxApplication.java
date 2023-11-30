0fcccaeecad2af69a3a29464ccd1f40464e7014b
KD <kdpembile@securitybank.com.ph>
2023-11-30 11:30:16 +0800

    Initial Commit

---
+++ demo-for-spring-webflux/src/main/java/com/kentisthebest/DemoForSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForSpringWebfluxApplication.class, args);
+	}
+
+}
